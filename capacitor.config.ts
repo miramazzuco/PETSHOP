6d4af7c86afd36eb88b158edca5bda1ca401cee4
miramazzuco <mirelamazzuco2006@gmail.com>
2023-06-22 12:26:14 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PETSHOP',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
